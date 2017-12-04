2f506e92c6773211f4be1ad093f5d7af769d9add
Aaron Freidus <aaronf101@gmail.com>
2017-12-04 08:53:52 -0500

    challenge 1 done'

--- day-4.py
+++ day-4.py
@@ -1,4 +1,19 @@
-input = open('./inputs/day4.txt', 'r').readlines()
+input = [line.rstrip() for line in open('./inputs/day4.txt')]
+badWords = 0
 
 for line in input: 
-  
+  dictionary = {}
+  words = line.split(' ')
+  for word in words:
+    print(words)
+    if word in dictionary:
+      badWords = badWords + 1
+      break
+    dictionary[ word ] = 1
+
+print(len(input) - badWords)
+
+
+444
+
+456
