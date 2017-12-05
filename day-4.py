f2255d23f7c6c30ab7178cd015923c7161573a41
Aaron Freidus <aaronf101@gmail.com>
2017-12-04 20:14:07 -0500

    second solution for day-4

--- day-4.py
+++ day-4.py
@@ -14,3 +14,22 @@ for line in input:
     dictionary[ word ] = 1
 
 print(len(input) - badWords)
+
+
+
+# ---- CHALLENGE 2 ------
+badCount = 0
+
+def letterMap(word=''):
+  mp = {}
+  for i, letter in enumerate(''.join(sorted(word))):
+    if letter not in mp: mp[ letter ] = 0
+    mp[ letter ] = mp[ letter ] + 1
+  return mp
+  
+for line in input:
+  words = line.split(' ')
+  words = [ letterMap(word) for word in words ]
+  if len(words) != len([dict(t) for t in set([tuple(d.items()) for d in words])]):
+    badCount = badCount + 1
+print(len(input) - badCount)
