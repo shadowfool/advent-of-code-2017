1259fdb78422de1dad2720f0c1370e53a50fdc36
Aaron Freidus <afreidus@blueraster.com>
2017-12-12 12:37:22 -0500

    add files missing

--- day-7.py
+++ day-7.py
@@ -1,18 +1,23 @@
 input = [line.rstrip() for line in open('./inputs/day7.txt')]
 
 dict = {}
-
+#### CHEESED PART 1 #####
 for line in input:
 	stripStart = line.index('(')
 	stripEnd = line.index('>') if '>' in line else line.index(')')
 	line = line[:stripStart] + line[stripEnd + 1:]
 	line = line.replace(',', '')
 	line = line.split(' ')
-	print(line)
 	for word in line: 
 		if word in dict: dict[word] += 1
 		else:
 			dict[ word ] = 1
 
 
-print(dict)
+##### PART 2 #######
+dict2 = {}
+comb = {}
+
+input2 = [ line.rstrip() for line in open('./inputs/day7.txt') ]
+
+
